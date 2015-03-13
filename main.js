1cbdc2e311896770d34689b5d788e568fce62ad1
teivaz <teivaz@rambler.ru>
2015-03-13 17:33:40 +0200

    - Fixed html5 project config

--- main.js
+++ main.js
@@ -52,7 +52,7 @@ cc.game.onStart = function(){
         document.body.removeChild(document.getElementById("cocosLoading"));
 
     cc.view.adjustViewPort(true);
-    cc.view.setDesignResolutionSize(800, 450, cc.ResolutionPolicy.SHOW_ALL);
+    cc.view.setDesignResolutionSize(1024, 768, cc.ResolutionPolicy.SHOW_ALL);
     cc.view.resizeWithBrowserSize(true);
     //load resources
     cc.LoaderScene.preload(g_resources, function () {
